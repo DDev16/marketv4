35aec73703e27e5aa39cbd465953b37a73ba1cb4
DDev16 <dillondevera12@gmail.com>
2023-07-17 18:14:29 -0700

    Refactor: Improved performance of sorting algorith

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-ReactDOM.createRoot(
-  document.getElementById('root')
-).render(<App />);
+createRoot(document.getElementById('root')).render(<App />);
